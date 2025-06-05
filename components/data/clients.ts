cdd204e555d22f901a64af6d149c2fb32d41185c
imtify <imti.bh@gmail.com>
2025-06-05 11:53:51 +0600

    delete data

--- components/data/clients.ts
+++
@@ -1,9 +0,0 @@
-export const clients = [
-  { src: "/client1.svg", alt: "Client 1" },
-  { src: "/client2.svg", alt: "Client 2" },
-  { src: "/client3.svg", alt: "Client 3" },
-  { src: "/client4.svg", alt: "Client 4" },
-  { src: "/client5.svg", alt: "Client 5" },
-  { src: "/client6.svg", alt: "Client 6" },
-  { src: "/client7.svg", alt: "Client 7" },
-];
