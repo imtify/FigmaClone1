898b677de526e50f207b230ac1533b9242e981fe
imtify <imti.bh@gmail.com>
2025-06-18 23:02:37 +0600

    Header refactor

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 function Header() {
   return (
     <header className="w-full bg-gray-50">
-      <div className="flex items-center justify-between wrapper !pl-[105px] !pr-[120px] py-[30px] h-[84px] mx-auto">
+      <div className="wrapper flex items-center justify-between !pl-[105px] !pr-[120px] py-[30px] h-[84px] mx-auto">
         <div className="flex items-center space-x-2">
           <Image
             src="/logo.svg"
@@ -17,13 +17,13 @@ function Header() {
           <Image
             src="/logo2.svg"
             alt="Logo"
-            width={112}
-            height={20}
+            width={111}
+            height={18}
             className="object-contain"
           />
         </div>
 
-        <nav className="hidden w-[425px] h-[30px] xl:flex items-center space-x-8">
+        <nav className="hidden w-[588px] h-[24px] xl:flex items-center space-x-[50px] ml-[42px]">
           <a
             href="#"
             className="font-medium text-base leading-6 tracking-normal text-gray-600 hover:text-gray-800"
@@ -62,14 +62,14 @@ function Header() {
           </a>
         </nav>
 
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-[14px]">
           <Button
             variant="ghost"
-            className="font-medium text-[14px] leading-5 tracking-normal text-green-500"
+            className="font-medium text-[14px] leading-5 tracking-normal text-green-500 px-[20.5px]"
           >
             Login
           </Button>
-          <Button className="font-medium text-[14px] leading-5 tracking-normal bg-green-500 hover:bg-green-600 text-white">
+          <Button className="font-medium text-[14px] leading-5 tracking-normal bg-green-500 hover:bg-green-600 text-white px-[20.5px]">
             Sign up
           </Button>
         </div>
