5f525a60ff63e05c1481a270fcd67bb736e6a8fc
imtify <imti.bh@gmail.com>
2025-06-19 13:08:48 +0600

    add css for small screen

--- components/sections/OurClientSection.tsx
+++ components/sections/OurClientSection.tsx
@@ -8,11 +8,11 @@ function OurClientSection() {
         <h2 className="text-4xl font-semibold leading-[44px] text-gray-800 mb-4">
           Our Clients
         </h2>
-        <p className="text-gray-600 mb-12">
+        <p className="text-gray-600 lg:mb-12 mb-0">
           We have been working with some Fortune 500+ clients
         </p>
       </div>
-      <div className="flex flex-wrap sm:flex-nowrap justify-center items-center space-x-[40px] md:space-x-[80px] 2xl:space-x-[136px]">
+      <div className="flex flex-wrap sm:flex-nowrap justify-center items-center space-x-[10px] md:space-x-[80px] 2xl:space-x-[136px]">
         {clients.map((client, i) => (
           <div key={i} className="w-12 h-12 flex items-center justify-center">
             <img
