6a22102ae6e22d1ace8aa459d3f7d35ff974d4a4
imtify <imti.bh@gmail.com>
2025-06-19 11:24:59 +0600

    div change of OurClient section

--- components/sections/OurClientSection.tsx
+++ components/sections/OurClientSection.tsx
@@ -11,18 +11,17 @@ function OurClientSection() {
         <p className="text-gray-600 mb-12">
           We have been working with some Fortune 500+ clients
         </p>
-
-        <div className="flex flex-wrap sm:flex-nowrap justify-center items-center space-x-[40px] md:space-x-[80px] 2xl:space-x-[140px]">
-          {clients.map((client, i) => (
-            <div key={i} className="w-12 h-12 flex items-center justify-center">
-              <img
-                src={client.src}
-                alt={client.alt}
-                className="object-contain w-full h-full"
-              />
-            </div>
-          ))}
-        </div>
+      </div>
+      <div className="flex flex-wrap sm:flex-nowrap justify-center items-center space-x-[40px] md:space-x-[80px] 2xl:space-x-[136px]">
+        {clients.map((client, i) => (
+          <div key={i} className="w-12 h-12 flex items-center justify-center">
+            <img
+              src={client.src}
+              alt={client.alt}
+              className="object-contain w-full h-full"
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
